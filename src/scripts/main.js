e7702adcf0efc7221992e3a9c7b8cfaa8308b3b0
Resolution-hash <62210636+Resolution-hash@users.noreply.github.com>
2022-12-11 14:53:44 +0300

    added night theme and weather animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,43 +3,53 @@
 const API_KEY = 'ba8a29562c22f982c837f087d414ba7a'
 const link = ['http://api.openweathermap.org/geo/1.0/direct?q=', 'https://api.openweathermap.org/data/2.5/forecast?']
 const month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+const animationElemmetns = document.querySelectorAll('.animation')
+const snowFlake_1 = document.getElementById('snow-1')
+const snowFlake_2 = document.getElementById('snow-2')
+const rain = document.getElementById('rain')
+const cloud = document.getElementById('cloud')
+const snowDescription = "снегопад"
+const snowDescriptionSmall = "небольшой снег"
+const rainDescription = "дождь"
+const rainDescriptionSmall = "небольшой дождь"
+const cloudDescription = "пасмурно"
+const cloudDescriptionSmall = "облачно с прояснениями"
+const cloudDescriptionSmall1 = "переменная облачность"
 
 
-// const destDecription = document.getElementById('current-description')
-// const destTemp = document.getElementById('current-temp')
-// const destTempMax = document.getElementById('current-max')
-// const destTempMin = document.getElementById('current-min')
-// const destFeelsLike = document.getElementById('current-feelsLike')
-// const desthumidity = document.getElementById('current-humidity')
-// const destDate = document.getElementById('current-date')
 
-let today = new Date()
 
 let store = {
   city: '',
   lat: 0,
   lon: 0,
+  timeOfDay: 'd',
 
   updateLocation: function (city, lat, lon) {
     this.city = city
     this.lat = lat.toFixed(2)
     this.lon = lon.toFixed(2)
+  },
+
+  updateTimeOfDay: function (pod) {
+    this.timeOfDay = pod
   }
 }
 
 class Day {
   constructor(options) {
     this.description = options.description,
-    this.temp = options.temp,
-    this.date = options.date
+      this.temp = options.temp,
+      this.date = options.date
     this.tempMax = options.tempMax,
-    this.tempMin = options.tempMin,
-    this.feelsLike = options.feelsLike,
-    this.humidity = options.humidity
+      this.tempMin = options.tempMin,
+      this.feelsLike = options.feelsLike,
+      this.humidity = options.humidity
     this.icon = options.icon
+    this.background = options.background
   }
 
-  updateIndicators(description, temp, tempMax, tempMin, feelsLike, humidity, date, icon) {
+  updateIndicators(description, temp, tempMax, tempMin, feelsLike, humidity, date, icon, pod) {
     this.description.textContent = description
     this.temp.textContent = addDeg(convertToCel(temp))
     this.tempMax.textContent = addDeg(convertToCel(tempMax))
@@ -48,6 +58,7 @@ class Day {
     this.humidity.textContent = addPer(humidity)
     this.date.textContent = dateTransform(date)
     this.icon.src = `./images/${icon}.png`
+    this.background.classList.add(this.checkTimeOfDay(pod))
   }
 
   updateIndicatorsNext(description, temp, date, icon) {
@@ -57,11 +68,107 @@ class Day {
     this.icon.src = `./images/${icon}.png`
   }
 
+  checkWeather(description) {
+
+    switch (description) {
+      case snowDescription:
+        snowFlake_1.classList.remove('disable')
+        snowFlake_2.classList.remove('disable')
+        break
+      case snowDescriptionSmall:
+        snowFlake_1.classList.remove('disable')
+        snowFlake_2.classList.remove('disable')
+        break
+      case rainDescription:
+        rain.classList.remove('disable')
+        break
+      case rainDescriptionSmall:
+        rain.classList.remove('disable')
+        break
+      case cloudDescription:
+        cloud.classList.remove('disable')
+        break
+      case cloudDescriptionSmall:
+        cloud.classList.remove('disable')
+      case cloudDescriptionSmall1:
+        cloud.classList.remove('disable')
+        break
+
+    }
+  }
+  checkTimeOfDay(pod) {
+    const weatherHeder = document.querySelectorAll('.weather-header')
+    const formInput = document.getElementById('form-input')
+    const button = document.getElementById('button')
+    if (pod === 'n') {
+      currentDay.background.classList.remove('day')
+      setfont('font-night', 'font-night-secondary')
+      setBorderColor(weatherHeder, 'border-night')
+      formInput.classList.add('border-night')
+      button.classList.add('border-night')
+      button.classList.add('font-night-secondary')
+      return 'night'
+    } else {
+      currentDay.background.classList.remove('night')
+      setfont('font-day', 'font-day-secondary')
+      unsetBorderColor(weatherHeder, 'border-night')
+      formInput.classList.remove('border-night')
+      button.classList.remove('border-night')
+      button.classList.remove('font-night-secondary')
+      return 'day'
+    }
+
+    function setBorderColor(elem, className){
+      elem.forEach((el) => {
+        el.classList.add(className)
+      })
+    }
+
+    function unsetBorderColor(elem, className){
+      elem.forEach((el) => {
+        el.classList.remove(className)
+      })
+    }
+
+    function setfont(className, classNameSecondary, classNameBorder) {
+      const formLabel = document.getElementById('form-label')
+      const weatherTitle = document.querySelectorAll('.weather-footer__title')
+      const body = document.body
+      const classDay = 'font-day-secondary'
+      const classNight = 'font-night-secondary'
+
+      
+
+      for (let i = 0; i < weatherTitle.length; i++) {
+        if (weatherTitle[i].classList.contains(classDay)) {
+          weatherTitle[i].classList.remove(classDay)
+        } else if (weatherTitle[i].classList.contains(classNight)) {
+          weatherTitle[i].classList.remove(classNight)
+        }
+        weatherTitle[i].classList.add(classNameSecondary)
+      }
+
+      if (formLabel.classList.contains(classDay)) {
+        formLabel.classList.remove(classDay)
+      } else if (formLabel.classList.contains(classNight)) {
+        formLabel.classList.remove(classNight)
+      }
+
+      if (formInput.classList.contains(classDay)) {
+        formInput.classList.remove(classDay)
+      } else if (formInput.classList.contains(classNight)) {
+        formInput.classList.remove(classNight)
+      }
+
+      formLabel.classList.add(classNameSecondary)
+      formInput.classList.add(classNameSecondary)
+      formInput.classList.toggle(classNameBorder)
+      body.classList.remove(...body.classList)
+      body.classList.add(className)
+    }
+
+  }
 }
-
-
-
-
 const currentDay = new Day({
   description: document.getElementById('current-description'),
   temp: document.getElementById('current-temp'),
@@ -70,11 +177,9 @@ const currentDay = new Day({
   feelsLike: document.getElementById('current-feelsLike'),
   humidity: document.getElementById('current-humidity'),
   date: document.getElementById('current-date'),
-  icon: document.getElementById('current-icon')
+  icon: document.getElementById('current-icon'),
+  background: document.getElementById('main')
 })
-
-console.log(currentDay)
-
 
 const tomorrowDay = new Day({
   description: document.getElementById('tomorrow-description'),
@@ -82,7 +187,6 @@ const tomorrowDay = new Day({
   date: document.getElementById('tomorrow-date'),
   icon: document.getElementById('tomorrow-icon')
 })
-console.log(tomorrowDay)
 
 
 const tomorrowAfterDay = new Day({
@@ -92,80 +196,105 @@ const tomorrowAfterDay = new Day({
   icon: document.getElementById('tomorrowAfter-icon')
 })
 
-console.log(tomorrowAfterDay)
 
-// const currentDay = {
-//   temp: document.getElementById('current-temp'),
-//   description: document.getElementById('current-description'),
-//   tempMax: document.getElementById('current-max'),
-//   tempMin: document.getElementById('current-min'),
-//   feelsLike: document.getElementById('current-feelsLike'),
-//   humidity: document.getElementById('current-humidity'),
-//   date: document.getElementById('current-date')
-// }
+function geoFind() {
+  const app = document.getElementById('app')
+  const status = document.getElementById('status')
 
+  function success(position) {
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
 
+    status.textContent = ''
+    fetchData(latitude, longitude)
+    console.log('+')
+  }
 
+  function error() {
+    status.textContent = 'Невозможно получить местоположение'
+  }
 
-// function geoFind() {
-//   const app = document.getElementById('app')
-//   const status = document.getElementById('status')
+  if (!navigator.geolocation) {
+    status.textContent = 'Геолокация не поддерживается вашим браузером'
 
-//   function success(position){
-//     const latitude = position.coords.latitude
-//     const longitude = position.coords.longitude
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error)
+  }
 
-//     status.textContent = ''
-//     fetchData(latitude,longitude,place)
-//   }
+}
+geoFind()
 
-//   function error(){
-//     status.textContent = 'Невозможно получить ваше местоположение'
-//   }
-
-//   if(!navigator.geolocation){
-//     status.textContent = 'Геолокация не поддерживается вашим браузером'
-
-//   } else {
-//     navigator.geolocation.getCurrentPosition(success, error)
-//   }
-
-// }
-// geoFind()
-
-// const fetchData = async (latitude, longitude, place) => {
-//   result = await fetch(`${link}/data/2.5/weather?lat=${store.lat}&lon=${store.lon}&lang=ru&appid=${API_KEY}`)
-//     .then(stat)
-//     .then(json)
-//     .then(data => {
-//       let {
-//         main: {
-//           temp: temp,
-//           feels_like: feelsLike,
-//           humidity: humidity,
-//           temp_max: tempMax,
-//           temp_min: tempMin,
-//         }
-//       } = data
-
-//       store = {
-//         ...store,
-//         temp: convertToCel(temp),
-//         tempMax: convertToCel(tempMax),
-//         tempMin: convertToCel(tempMin),
-//         feelsLike: convertToCel(feelsLike),
-//         humidity,
-//       }
-
-//       console.log(store)
-//     })
-//     .catch(error => {
-//       console.log(error)
-//     })
-
-// }
+const fetchData = async (lat, lon) => {
+  let result = await fetch(`${link[1]}lat=${lat}&lon=${lon}&lang=ru&appid=${API_KEY}`)
+    .then(stat)
+    .then(json)
+    .then(data => {
+      console.log(data)
+      let {
+        city: {
+          name
+        },
+        list:
+        {
+          0: {
+            main: {
+              temp,
+              humidity,
+              feels_like: feelsLike,
+              temp_max: tempMax,
+              temp_min: tempMin,
+            },
+            dt_txt: date,
+            weather: {
+              0: {
+                description,
+                icon
+              }
+            },
+            sys: {
+              pod
+            }
+          },
+          8: {
+            main: {
+              temp: tomorrowTemp
+            },
+            dt_txt: tomorrowDate,
+            weather: {
+              0: {
+                description: tomorrowDescription,
+                icon: tomorrowIcon
+              }
+            }
+          },
+          16: {
+            main: {
+              temp: tomorrowAfterTemp
+            },
+            dt_txt: tomorrowAfterDate,
+            weather: {
+              0: {
+                description: tomorrowAfterTempDescription,
+                icon: tomorrowAfterIcon
+              }
+            }
+          }
+        }
+      } = data
+      labelSet(name)
+      currentDay.updateIndicators(description, temp, tempMax, tempMin, feelsLike, humidity, date, icon, pod)
+      currentDay.checkWeather(description)
+      tomorrowDay.updateIndicatorsNext(tomorrowDescription, tomorrowTemp, tomorrowDate, tomorrowIcon)
+      tomorrowAfterDay.updateIndicatorsNext(tomorrowAfterTempDescription, tomorrowAfterTemp, tomorrowAfterDate, tomorrowAfterIcon)
+      dateTransform(date)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
 
 const fetchCity = async (place) => {
+  setClassDisable()
   let result = await fetch(`${link[0]}${place}&appid=${API_KEY}`)
   let data = await result.json()
   let {
@@ -210,6 +339,9 @@ const fetchCity = async (place) => {
                 description: tomorrowDescription,
                 icon: tomorrowIcon
               }
+            },
+            sys: {
+              pod
             }
           },
           16: {
@@ -227,41 +359,12 @@ const fetchCity = async (place) => {
         }
       } = data
       labelSet(name)
-      currentDay.updateIndicators(description, temp, tempMax, tempMin, feelsLike, humidity, date, icon)
+      currentDay.updateIndicators(description, temp, tempMax, tempMin, feelsLike, humidity, date, icon, pod)
+      currentDay.checkWeather(description)
       tomorrowDay.updateIndicatorsNext(tomorrowDescription, tomorrowTemp, tomorrowDate, tomorrowIcon)
       tomorrowAfterDay.updateIndicatorsNext(tomorrowAfterTempDescription, tomorrowAfterTemp, tomorrowAfterDate, tomorrowAfterIcon)
-      console.log(currentDay)
-      console.log(tomorrowDay)
-      console.log(tomorrowAfterDay)
       dateTransform(date)
     })
-
-  // result = await fetch(`${link[1]}lat=${store.lat}&lon=${store.lon}&appid=${API_KEY}`)
-  //   .then(stat)
-  //   .then(json)
-  //   .then(data => {
-  //     let {
-  //       weather: [
-  //         {
-  //           description: description,
-  //         }
-  //       ],
-  //       main: {
-  //         temp: temp,
-  //         feels_like: feelsLike,
-  //         humidity: humidity,
-  //         temp_max: tempMax,
-  //         temp_min: tempMin,
-  //       }
-  //     } = data
-  //     store.updateIndicators(description, temp, tempMax, tempMin, feelsLike, humidity)
-  //     updateWeather(place)
-
-  //     console.log(store)
-  //   })
-  //   .catch(error => {
-  //     console.log(error)
-  //   })
 }
 
 document.forms.checkWeather.onsubmit = function (e) {
@@ -272,47 +375,13 @@ document.forms.checkWeather.onsubmit = function (e) {
 }
 
 function labelSet(value) {
-  console.log(value)
   const labelVal = document.getElementById('label-city')
   labelVal.textContent = value
-  console.log(1)
 }
 
-// const updateWeather = function (value) {
-//   // label update
-//   const content = ucFirst(value)
-//   labelVal.textContent = content
-//   //temp update
-//   currentDay.temp.textContent = addDeg(store.temp)
-//   //tempMax update
-//   currentDay.tempMax.textContent = addDeg(store.tempMax)
-//   //tempMin update
-//   currentDay.tempMin.textContent = addDeg(store.tempMin)
-//   //description update
-//   currentDay.description.textContent = store.description
-//   //humidity update
-//   currentDay.humidity.textContent = addPer(store.humidity)
-//   //feelsLike update 
-//   currentDay.feelsLike.textContent = addDeg(store.feelsLike)
-// }
 
-// function updateDate(date) {
-//   let currentMonth = ''
-
-//   const currentDay = today.getUTCDate()
-//   const currentYear = today.getFullYear()
-
-//   for (let i = 0; i < month.length; i++) {
-//     let value = today.getMonth()
-//     currentMonth = month[value]
-//   }
-//   date.textContent = `${currentMonth} ${currentDay}, ${currentYear}`
-
-// }
-// updateDate(currentDay.date)
 function dateTransform(data) {
   let date = data.slice(0, 10)
-  console.log(date)
   const current_year = date.slice(0, 4)
   let current_month = date.slice(5, 7)
   let current_day = date.slice(8, 10)
@@ -372,12 +441,24 @@ function ucFirst(str) {
   return str[0].toUpperCase() + str.slice(1);
 }
 
+function setClassDisable() {
+  animationElemmetns.forEach((elem) => {
+    elem.classList.add('disable')
+  })
+}
 
 
-
-
-
-
+document.body.onload = function () {
+  setTimeout(function () {
+    {
+      let preloader = document.getElementById('page-preloader')
+      console.log(preloader)
+      if (!preloader.classList.contains('done')) {
+        preloader.classList.add('done')
+      }
+    }
+  }, 1000)
+}
 
 
 
